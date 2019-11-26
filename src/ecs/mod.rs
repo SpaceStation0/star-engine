3613b5ec2ca97862fa495fab244bfe54de3af2c3
Jackson Lewis <inquisitivepenguin@protonmail.com>
2019-11-26 16:49:05 +0000

    Completely rewrite ECS framework

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -3,22 +3,17 @@
 //! easily extendable, and endlessly configurable.
 //! Everything that happens inside the game is controlled from here.
 //!
-//! Star Engine has a tick-based design. Each tick, a scheduler
-//! designates what systems should be executed. By default, this
-//! scheduler will read the execution policies of systems and
-//! execute them however often they need to be executed.
-//! But the scheduler can be configured and adjusted to better
-//! suit the needs of a certain application.
+//! Star Engine has a tick-based design. Each system
+//! has an execution policy and is responsible for knowing when
+//! to execute.
 //!
 //! Everything that happens occurs in the `Game` class. This
 //! is the container for all sub-elements of the game. These
 //! sub-elements include:
 //! - The entity world
-//! - The scheduler
-//! - The system dispatcher
+//! - The systems dispatcher
 //! - The resource allocator
 //! - The client handler
-//! - The high-level game controllers
 //! - The scripting engine
 //!
 //! Usually, the main function of an actual game
@@ -28,19 +23,69 @@
 //! Each sub-element of the `Game` struct is documented in
 //! each of their respective folders.
 
-pub mod world;
-pub mod system;
+use specs::{World, Dispatcher, DispatcherBuilder};
+use specs::shred::ParSeq;
+use super::script::PythonInterpreter;
+
 pub mod event;
 pub mod notifier;
-pub mod scheduler;
-pub mod accessor;
-use world::World;
-use scheduler::Scheduler;
+pub mod network;
 
 /// The main struct, from which all game execution
 /// occurs. A container of all engine elements.
-pub struct Game {
-    entity_world: World,
+pub struct Game<'a, 'b, P, T> {
+    world: World,
+    dispatcher: Dispatcher<'a, 'b>,
+    notify_dispatcher: ParSeq<P, T>,
 
 }
 
+pub struct GameBuilder<'a, 'b, P, T> {
+    world: World,
+    dispatcher: DispatcherBuilder<'a, 'b>,
+    notify_dispatcher: Option<ParSeq<P, T>>
+}
+
+impl<'a, 'b, P, T> Game<'a, 'b, P, T> {
+    pub fn new() -> GameBuilder<'a, 'b, P, T> {
+        GameBuilder {
+            world: World::new(),
+            dispatcher: DispatcherBuilder::new(),
+            notify_dispatcher: None
+        }
+    }
+
+    //TODO: Add server options
+    pub fn start_server(&mut self) {
+
+    }
+
+    pub fn tick(&mut self) {
+
+    }
+
+    pub fn status(&self) {
+
+    }
+
+    pub fn reboot(&mut self) {
+
+    }
+}
+
+impl<'a, 'b, P, T> Game<'a, 'b, P, T> {
+    async fn internal_tick(&mut self) -> Result<(), String> {
+        Ok(())
+    }
+
+    // Initialize the global state
+    fn initialize(&mut self) {
+
+    }
+
+    fn clear_world(&mut self) {
+        let mut world = World::new();
+        std::mem::swap(&mut self.world, &mut world);
+        drop(world);
+    }
+}
