29b1a949422d949e7c0ea997fa73ebc56062baae
Jackson Lewis <inquisitivepenguin@protonmail.com>
2019-12-18 13:45:52 +0000

    Remove useless feature declarations

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,21 @@
 #![allow(dead_code)]
-#![feature(try_from)]
 #![feature(trait_alias)]
 #![feature(type_ascription)]
-#![feature(async_await)]
 #![feature(async_closure)]
+//#![warn(clippy::pedantic)]
 
 extern crate specs;
-#[macro_use]
 extern crate cascade;
-#[macro_use]
 extern crate lazy_static;
-#[macro_use]
 extern crate tokio;
-#[macro_use]
 extern crate futures;
 extern crate bytes;
-#[macro_use]
 pub extern crate cpython;
 
 pub mod network;
 pub mod ecs;
 pub mod script;
+pub mod logger;
 
 #[cfg(test)]
 mod tests;
