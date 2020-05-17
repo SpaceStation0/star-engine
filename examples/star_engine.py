db886bb3b749a98ff879a3aa82b1e85a710cd697
Jackson Lewis <inquisitivepenguin@protonmail.com>
2020-05-17 16:52:59 +0100

    Add doc comments to star_engine.py

--- examples/star_engine.py
+++ examples/star_engine.py
@@ -1,14 +1,23 @@
 class Read:
+    """
+    An accessor to read a global resource
+    """
     def __init__(self, class_name):
         self.class_name = class_name
 
 
 class Write:
+    """
+    An accessor to read/write a global resource
+    """
     def __init__(self, class_name):
         self.class_name = class_name
 
 
 class Entities:
+    """
+    An accessor to read all entities in the world
+    """
     def __init__(self):
         pass
     '''
@@ -19,6 +28,9 @@ class Entities:
 
 
 class System:
+    """
+    A system is a class which modifies a certain set of components each tick of the application
+    """
     @staticmethod
     def data():
         pass
